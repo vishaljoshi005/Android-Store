f224290c0395f1e9f95ef36df5f44d641b3b2f63
Vishal Joshi <vishaljoshi005@gmail.com>
2019-02-17 22:58:32 +0530

    final lesson adapter done

--- app/src/main/java/in/ivishaljoshi/mystore/Controller/ProductsActivity.kt
+++ app/src/main/java/in/ivishaljoshi/mystore/Controller/ProductsActivity.kt
@@ -1,17 +1,46 @@
 package `in`.ivishaljoshi.mystore.Controller
 
+import `in`.ivishaljoshi.mystore.Adapters.ProductRecycleView
 import `in`.ivishaljoshi.mystore.R
+import `in`.ivishaljoshi.mystore.Services.DataService
 import `in`.ivishaljoshi.mystore.Utilities.EXTRA_CATEGORY
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter:ProductRecycleView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+        adapter = ProductRecycleView(this, DataService.getProducts(categoryType))
 
+
+        var spanCount=2
+        var screenSize = resources.configuration.screenWidthDp
+        var orientation = resources.configuration.orientation
+
+        if(orientation==Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 3
+        }
+
+        if(screenSize>720){
+            spanCount = 3
+        }
+
+
+
+
+
+        val layoutManager = GridLayoutManager(this,spanCount)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
     }
 }
--- app/src/main/java/in/ivishaljoshi/mystore/Services/DataService.kt
+++ app/src/main/java/in/ivishaljoshi/mystore/Services/DataService.kt
@@ -23,10 +23,19 @@ object DataService {
         Product("Devslopes Graphic Beanie", "$18", "hat1"),
         Product("Devslopes Hat Black", "$20", "hat2"),
         Product("Devslopes Hat White", "$18", "hat3"),
+        Product("Devslopes Hat Snapback", "$22", "hat4"),
+        Product("Devslopes Graphic Beanie", "$18", "hat1"),
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat White", "$18", "hat3"),
         Product("Devslopes Hat Snapback", "$22", "hat4")
+
     )
 
     val hoodies = listOf(
+        Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
+        Product("Devslopes Hoodie Red", "$32", "hoodie2"),
+        Product("Devslopes Gray Hoodie", "$28", "hoodie3"),
+        Product("Devslopes Black Hoodie", "$32", "hoodie4"),
         Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
         Product("Devslopes Hoodie Red", "$32", "hoodie2"),
         Product("Devslopes Gray Hoodie", "$28", "hoodie3"),
@@ -34,6 +43,11 @@ object DataService {
     )
 
     val shirts = listOf(
+        Product("Devslopes Shirt Black", "$18", "shirt1"),
+        Product("Devslopes Badge Light Gray", "$20", "shirt2"),
+        Product("Devslopes Logo Shirt Red", "$22", "shirt3"),
+        Product("Devslopes Hustle", "$22", "shirt4"),
+        Product("Kickflip Studios", "18", "shirt5"),
         Product("Devslopes Shirt Black", "$18", "shirt1"),
         Product("Devslopes Badge Light Gray", "$20", "shirt2"),
         Product("Devslopes Logo Shirt Red", "$22", "shirt3"),
@@ -46,10 +60,10 @@ object DataService {
 
     fun getProducts(category:String): List<Product>{
         return when(category){
-            "shirts" -> shirts
-            "hoodies" -> hoodies
-            "hats"-> hats
+            "SHIRTS" -> shirts
+            "HOODIES" -> hoodies
+            "HATS"-> hats
             else-> digitalGoods
         }
     }
-}  
+}
