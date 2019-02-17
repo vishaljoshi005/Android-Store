d2792277293423b007c55a1b5f1ebc90de9b0626
Vishal Joshi <vishaljoshi005@gmail.com>
2019-02-17 19:30:29 +0530

    Changes made to products

--- app/src/main/java/in/ivishaljoshi/mystore/Controller/ProductsActivity.kt
+++ app/src/main/java/in/ivishaljoshi/mystore/Controller/ProductsActivity.kt
@@ -1,5 +1,7 @@
 package `in`.ivishaljoshi.mystore.Controller
 
+import `in`.ivishaljoshi.mystore.R
+import `in`.ivishaljoshi.mystore.Utilities.EXTRA_CATEGORY
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +10,8 @@ class ProductsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
+
+        val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+
     }
 }
